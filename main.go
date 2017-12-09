ebd480695fca236917115557e516fa00f8ea153d
Shun Hosaka <reyurnible@gmail.com>
2017-12-10 02:00:43 +0900

    add option parser

--- main.go
+++ main.go
@@ -1,13 +1,39 @@
 package main
 
 import (
+	"fmt"
+	"os"
 	"sync"
 	"time"
+
+	flags "github.com/jessevdk/go-flags"
 )
 
+type Options struct {
+	Room     string `short:"r" long:"room" description:"Room name" required:"true"`
+	Password string `short:"p" long:"pass" description:"Password"`
+	Listen   uint   `short:"l" long:"listen" description:"Using port number"`
+}
+
 var wg sync.WaitGroup
+var options Options
 
 func main() {
+	var parser = flags.NewParser(&options, flags.Default)
+	if _, err := parser.Parse(); err != nil {
+		if flagsErr, ok := err.(*flags.Error); ok && flagsErr.Type == flags.ErrHelp {
+			os.Exit(0)
+		} else {
+			os.Exit(1)
+		}
+	}
+	fmt.Printf("Room: %s\n", options.Room)
+	fmt.Printf("Password: %s\n", options.Password)
+	fmt.Printf("Listen: %d\n", options.Listen)
+	sleigh()
+}
+
+func sleigh() {
 	showTextLogo()
 	showLogo()
 	/*
