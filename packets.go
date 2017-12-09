93cff92eb978be664704093ef897a273747d580c
iwate <iwatesoftware@live.jp>
2017-12-09 16:31:07 +0900

    add checksum index encode/decode

--- packets.go
+++ packets.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"bytes"
+	"encoding/binary"
+	"io"
+
+	"github.com/Redundancy/go-sync/chunks"
+	"github.com/Redundancy/go-sync/filechecksum"
+	"github.com/Redundancy/go-sync/index"
+)
+
 // Hey is first message packet through UDP multicast
 type Hey struct {
 	Hostname string `json:"hostname"`
@@ -45,3 +55,70 @@ const (
 	// Delete file|dir
 	Delete
 )
+
+// EncodeChecksumIndex encode ChecksumIndex of gosync
+func EncodeChecksumIndex(content io.Reader, fileSize int64, blockSize uint) (io.ReadSeeker, error) {
+	generator := filechecksum.NewFileChecksumGenerator(uint(blockSize))
+	weakSize := generator.WeakRollingHash.Size()
+	strongSize := generator.GetStrongHash().Size()
+	b := bytes.NewBuffer(nil)
+	b.Write(int64ToBytes(fileSize))
+	b.Write(intToBytes(weakSize))
+	b.Write(intToBytes(strongSize))
+	_, err := generator.GenerateChecksums(content, b)
+	if err != nil {
+		return nil, err
+	}
+
+	return bytes.NewReader(b.Bytes()), nil
+}
+
+// DecodeChecksumIndex decode EncodeChecksumIndex of gosync
+func DecodeChecksumIndex(reader io.Reader) (fileSize int64, idx *index.ChecksumIndex, lookup filechecksum.ChecksumLookup, err error) {
+	fBlock := bytes.NewBuffer(nil)
+	wBlock := bytes.NewBuffer(nil)
+	sBlock := bytes.NewBuffer(nil)
+
+	_, err = io.CopyN(fBlock, reader, 8)
+	if err != nil {
+		return
+	}
+
+	_, err = io.CopyN(wBlock, reader, 4)
+	if err != nil {
+		return
+	}
+
+	_, err = io.CopyN(sBlock, reader, 4)
+	if err != nil {
+		return
+	}
+
+	fileSize = int64(binary.LittleEndian.Uint64(fBlock.Bytes()))
+	weakSize := int(binary.LittleEndian.Uint32(wBlock.Bytes()))
+	strongSize := int(binary.LittleEndian.Uint32(sBlock.Bytes()))
+
+	readChunks, err := chunks.LoadChecksumsFromReader(reader, weakSize, strongSize)
+
+	if err != nil {
+		return
+	}
+
+	idx = index.MakeChecksumIndex(readChunks)
+	lookup = chunks.StrongChecksumGetter(readChunks)
+
+	return
+}
+
+// helpers
+func intToBytes(val int) []byte {
+	bs := make([]byte, 4)
+	binary.LittleEndian.PutUint32(bs, uint32(val))
+	return bs
+}
+
+func int64ToBytes(val int64) []byte {
+	bs := make([]byte, 8)
+	binary.LittleEndian.PutUint64(bs, uint64(val))
+	return bs
+}
